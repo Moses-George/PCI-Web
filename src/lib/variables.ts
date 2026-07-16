c4cb429ff3b8de2bbb046419d2a483eacecf621e
Moses-George <mosesgeorge323287@gmail.com>
2026-07-16 13:35:46 +0100

    General update

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,1 +1,2 @@
-export const VITE_ENDPOINT_URL = import.meta.env.VITE_ENDPOINT_URL;
+export const VITE_ENDPOINT_URL = "https://pci-server-production.up.railway.app/"
+// import.meta.env.VITE_ENDPOINT_URL;
